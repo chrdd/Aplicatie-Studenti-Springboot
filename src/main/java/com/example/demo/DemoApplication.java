6495cb569b2ae4a5c90222c050ba07bc7a7e77fa
Sandu Octavian Stefan <75358954+chrdd@users.noreply.github.com>
2023-07-18 14:32:27 +0300

    API

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,15 +11,11 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(new Student(1L,"Alin",21,"Salutare",LocalDate.of(2000,Month.AUGUST,5),"alin24@yahoo.com"));
-	}
+
 }
